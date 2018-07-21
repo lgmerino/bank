01f69fd366f773e75ec32e9e4708bbcdba904e38
lgmerino <lgmerino@yahoo.com>
2018-07-21 13:17:39 +0200

    New BankUser model

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,8 +4,8 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bank_local',  # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'bank_project',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BankUser(models.Model):
+    """
+    Model for users
+    first_name, last_name and iban are required
+    """
+    first_name = models.CharField(max_length=30, blank=False, verbose_name=_('First name'))
+    last_name = models.CharField(max_length=30, blank=False, verbose_name=_('Last name'))
+    iban = models.CharField(max_length=34, blank=False, verbose_name=_('IBAN'))
+    administrator = models.ForeignKey(User, null=False, blank=False, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = _(u'Bank user')
+        verbose_name_plural = _(u'Bank users')
+        ordering = ['first_name', 'last_name']
+
+    def __str__(self):
+        return u'%s %s (%s)' % (self.first_name, self.last_name, self.iban)
